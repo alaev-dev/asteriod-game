dafad74aec299af8e62ef23a54d53d8b4469d077
alaev <alaevdev@gmail.com>
2025-12-26 14:45:34 +0300

    player can move forward

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_SPEED, PLAYER_TURN_SPEED
 import pygame
 
 
@@ -29,3 +29,14 @@ class Player(CircleShape):
             self.rotate(dt * -1)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(dt * -1)
+
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_vector_with_speed = rotated_vector * PLAYER_SPEED * dt
+
+        self.position += rotated_vector_with_speed
