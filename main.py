bdce762e99fdce7a29dbce9a2272afd5159d8ad6
alaev <alaevdev@gmail.com>
2026-01-02 12:21:06 +0300

    add menus

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+import pygame_menu
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
@@ -7,17 +8,19 @@ from logger import log_event, log_state
 from player import Player
 from shot import Shot
 
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.init()
+pygame.font.init()
 
-def main():
+
+def start_the_game():
     print(f"Starting Asteroids woth pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
     dt = 0
+    score = 0
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
@@ -34,6 +37,7 @@ def main():
     Shot.containers = (shots, drawable, updatable)
 
     while True:
+        font = pygame.font.Font("monaco.ttf", 36)
         log_state()
 
         updatable.update(dt)
@@ -42,13 +46,14 @@ def main():
             if asteroid.collides_with(player):
                 log_event("player_hit")
                 print("Game over!")
-                sys.exit()
+                display_results(score)
 
             for shot in shots:
                 if shot.collides_with(asteroid):
                     log_event("asteroid_shot")
                     shot.kill()
                     asteroid.split()
+                    score += 10
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -59,9 +64,38 @@ def main():
         for item in drawable:
             item.draw(screen)
 
+        score_text = font.render(f"Score: {score}", True, (255, 255, 255))
+        screen.blit(score_text, (10, 10))
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
 
+def main_menu():
+    menu = pygame_menu.Menu(
+        "Welcome", SCREEN_WIDTH, SCREEN_HEIGHT, theme=pygame_menu.themes.THEME_DARK
+    )
+
+    menu.add.button("Play", start_the_game)
+    menu.add.button("Exit", pygame_menu.events.EXIT)
+
+    menu.mainloop(screen)
+
+
+def display_results(score_value):
+    results_menu = pygame_menu.Menu(
+        "Game Over",
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT,
+        theme=pygame_menu.themes.THEME_DARK,
+    )
+
+    results_menu.add.label(f"Your Score: {score_value}", font_size=30)
+    results_menu.add.button("Restart", start_the_game)
+    results_menu.add.button("Quit", pygame_menu.events.EXIT)
+
+    results_menu.mainloop(screen)
+
+
 if __name__ == "__main__":
-    main()
+    main_menu()
